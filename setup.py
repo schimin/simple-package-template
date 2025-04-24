ee8cc42ce85a7ce33f2807e5ad50404b176076df
schimin <robertojoaojr@gmail.com>
2025-04-24 13:21:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="package_name_roberto",
     version="0.0.1",
-    author="my_name",
+    author="roberto_schimin",
     author_email="my_email",
     description="My short description",
     long_description=page_description,
